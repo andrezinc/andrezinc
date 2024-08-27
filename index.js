663c9dacfda1ecdc52e29ebc3991482ab98e5fdc
Andre Castilhano <71910133+andrezinc@users.noreply.github.com>
2024-08-27 13:46:56 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+fs.readFile('README_BASE.md', 'utf-8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
